2dd2d88eb38f88e01ce17183ada924cb0e93a870
lukerhys <hello@lukemelvin.com>
2020-01-17 13:34:56 +0000

    Function 4

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,12 +8,12 @@ def total_cash(pet_shop_hash)
   return pet_shop_hash[:admin][:total_cash]
 end
 
-# #3.
+#3.
 def add_or_remove_cash(pet_shop_hash, money)
   return pet_shop_hash[:admin][:total_cash] += money
 end
 
-# #4.
-# def pets_sold()
-#
-# end
+#4.
+def pets_sold(pet_shop_hash)
+  return pet_shop_hash[:admin][:pets_sold]
+end
