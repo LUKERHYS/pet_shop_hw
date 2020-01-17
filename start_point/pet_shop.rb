feddc51cddcbedc8d857aba8e004d67b4db7d084
lukerhys <hello@lukemelvin.com>
2020-01-17 13:39:41 +0000

    Function 5

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -17,3 +17,8 @@ end
 def pets_sold(pet_shop_hash)
   return pet_shop_hash[:admin][:pets_sold]
 end
+
+#5.
+def increase_pets_sold(pet_shop_hash, number_of_pets)
+  return pet_shop_hash[:admin][:pets_sold] += number_of_pets
+end
