79a2648411fad83b57f5a9a280191a2ff874f780
lukerhys <hello@lukemelvin.com>
2020-01-17 14:49:17 +0000

    Function 7

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -22,3 +22,30 @@ end
 def increase_pets_sold(pet_shop_hash, number_of_pets)
   return pet_shop_hash[:admin][:pets_sold] += number_of_pets
 end
+
+#6.
+def stock_count(pet_shop_hash)
+  return pet_shop_hash[:pets].length()
+end
+
+#7.
+def pets_by_breed(pet_shop_hash, breed_of_animal)
+  pets_found = []
+  for pet in pet_shop_hash[:pets]
+      if pet[:breed] == breed_of_animal
+        pets_found.push(pet[:breed])
+      end
+  end
+  return pets_found
+end
+
+#8.
+def find_pet_by_name(pet_shop_hash, pet_name)
+  found_name = []
+  for name in pet_shop_hash[:pets]
+    if name[:name] == pet_name
+      found_name.push(name)
+    end
+  end
+  return found_name
+end
